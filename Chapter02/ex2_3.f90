c05a441337c7f09b85c335a1323a6c990578b2d4
Ray D. Sameshima <rds@rds.local>
2017-05-26 16:07:16 -0400

    square, cube and so on

--- Chapter02/ex2_3.f90
+++ Chapter02/ex2_3.f90
@@ -2,11 +2,19 @@
 ! Given integer n, return n, .. n**5.
 PROGRAM SquareToFifth
   INTEGER :: m, n
-  PRINT*, "Input an integer:"
-  READ*, n
+  PRINT *, "Input an integer:"
+  READ *, n
     m = n
     m = n**2; PRINT*, "(Input)**2 = ", m
     m = n**3; PRINT*, "(Input)**3 = ", m
     m = n**4; PRINT*, "(Input)**4 = ", m
     m = n**5; PRINT*, "(Input)**5 = ", m
 END PROGRAM SquareToFifth
+
+! $ ./ex2_3.out
+!  Input an integer:
+! 3
+!  (Input)**2 =            9
+!  (Input)**3 =           27
+!  (Input)**4 =           81
+!  (Input)**5 =          243
