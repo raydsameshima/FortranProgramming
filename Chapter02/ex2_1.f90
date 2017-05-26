9a3f243c17a09dc620c9cb9bf5d9fd90437a71df
Ray D. Sameshima <rds@rds.local>
2017-05-26 16:02:47 -0400

    add comments

--- Chapter02/ex2_1.f90
+++ Chapter02/ex2_1.f90
@@ -4,17 +4,18 @@
 
 PROGRAM Example_2_1
   REAL :: a, b, av1, av2, av3
-  PRINT*, "Given 2 numbers, will return these &
-           arithmetic, geometric, and harmonic means."
-  READ *, a,b
+  PRINT *, "Given 2 numbers, will return these &
+           arithmetic (a+b)/2, geometric sqrt(a*b), &
+           and harmonic 1/(1/a + 1/b) means."
+  READ  *, a, b
     av1 = (a + b)/2.0
-!   av2 = (a*b)**0.
+!   av2 = (a*b)**0.5
     av2 = sqrt(a*b)
     av3 = 1.0/((1.0/a + 1.0/b)/2.0)
   PRINT*, av1, av2, av3
 END PROGRAM Example_2_1
 
-! $ ./ex2_1 
-!  Given 2 numbers, will return thesearithmetic, geometric, and harmonic means.
-!  10 20
-!     15.0000000       14.1421356       13.3333330
+! $ ./ex2_1.out 
+!  Given 2 numbers, will return these arithmetic (a+b)/2, geometric sqrt(a*b), and harmonic 1/(1/a + 1/b) means.
+! 10 20
+!    15.0000000       14.1421356       13.3333330  
--- Chapter02/ex2_2.f90
+++ Chapter02/ex2_2.f90
@@ -4,10 +4,16 @@
 PROGRAM VolAndSur
   REAL :: pi, r , h, v, s
   pi = 3.141593
-  PRINT*, "Radius, height :"
-  READ*, r, h
+  PRINT *, "Radius, height :"
+  READ *, r, h
     v = (pi*h*r**2)/3.0
     s = pi*r*(r + (r**2 + h**2)**0.5)
-  PRINT*, "The volume is: v = ", v
-  PRINT*, "The surface are is: s = ", s
+  PRINT *, "The volume is: v =      ", v
+  PRINT *, "The surface are is: s = ", s
 END PROGRAM VolAndSur
+
+! $ ./ex2_2.out 
+!  Radius, height :
+! 100 1
+!  The volume is: v =         10471.9766    
+!  The surface are is: s =    62833.4336   
