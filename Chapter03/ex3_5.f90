47d73feb6df334c0217e56c5ed9ace92e9424450
Ray D. Sameshima <rds@rds.local>
2017-05-29 23:57:04 -0400

    made a folder

--- Chapter03/ex3_5.f90
+++ Chapter03/ex3_5.f90
@@ -1,9 +1,12 @@
 ! ex3_5.f90
 ! grading
 PROGRAM Grade
-  Integer :: i, n1, n2, n3, n4
-  n1=0; n2=0; n3=0; n4=0
+! Integer :: i, n1, n2, n3, n4
+! n1=0; n2=0; n3=0; n4=0
 
+  Integer :: i, n1=0, n2=0, n3=0, n4=0
+
+  Print *, "put a score (to quit, -1)"
 ! If the read is outside, goto 9
 1 Read *, i; If(i<0 .OR. i>100) Goto 9
 
