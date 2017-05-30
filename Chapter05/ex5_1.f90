4ea924dd24c4f4ad2039a1c9a58da377a19cabef
Ray D. Sameshima <rds@rds.local>
2017-05-30 12:19:50 -0400

    ex5_1 simple summation function with a loop

---
+++ Chapter05/ex5_1.f90
@@ -0,0 +1,19 @@
+! ex5_1.f90
+! square sum from 1 to 100
+PROGRAM SumOfSquares
+  Integer :: n, sum = 0
+
+  Do n = 1, 100
+    sum = sum + n*n
+  END Do
+
+  Print *, "sum of squre from 1 to 100 =", sum
+  Print *, "This time, the counter n should be: ", n
+
+END PROGRAM SumOfSquares
+!
+! ./ex5_1
+! sum of squre from 1 to 100 =      338350
+!
+! Prelude> sum $ map (^2) [1..100]
+! 338350
