5e610e895963c16c7ab56b6a65c7bb66e11d378c
Ray D. Sameshima <rds@rds.local>
2017-05-30 12:20:58 -0400

    ex5_2, double loop example

---
+++ Chapter05/ex5_2.f90
@@ -0,0 +1,18 @@
+! ex5_2.f90
+! KUKU, a multiplication table
+PROGRAM Kuku
+  Integer :: m, n, mn
+
+var1: Do m = 2,9 ! var1 is the label for the outer loop.
+var2:   Do n = 1,9
+          Print *, m, " times ", n, " ? Write your answer:"
+          Read *, mn
+          If(mn == m*n) Then
+            Print *, "ok"
+          Else
+            Print *, "wrong"
+          END If
+        END DO var2
+      END DO var1
+
+END PROGRAM Kuku
