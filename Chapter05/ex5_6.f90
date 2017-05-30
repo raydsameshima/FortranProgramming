6054b719e5606479c7a673f8190727a1f2f6eff6
Ray D. Sameshima <rds@rds.local>
2017-05-30 19:45:49 -0400

    ex5_6: primes using div-try.

---
+++ Chapter05/ex5_6.f90
@@ -0,0 +1,17 @@
+! ex5_6.j90
+! primes upto 100
+! trial division
+PROGRAM Prime100
+  Integer :: m, n, max !=100
+  print *, "Let us compute primes up to:"
+  read *, max
+  Print *, 2
+
+xxx: Do n = 3, max, 2
+       Do m = 3, Nint(Sqrt(Real(n))), 2
+         If(Mod(n,m)==0) Cycle xxx
+       End Do
+         Print *, n
+     End Do xxx
+
+END PROGRAM Prime100
