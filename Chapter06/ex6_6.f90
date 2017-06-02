7ce4aa49204bb09bd9e0400cc6c5f6b19fc7ec36
Ray D. Sameshima <rds@rds.local>
2017-06-02 09:39:33 +0900

    ex6_6 matrix product using loop explicitly

--- Chapter06/ex6_6.f90
+++ Chapter06/ex6_6.f90
@@ -3,18 +3,32 @@
 PROGRAM Matrix
   INTEGER, DIMENSION(:,:), ALLOCATABLE :: a,b,c ! 2 indices i.e. matrix
   INTEGER :: m=0, n=0, k=0, i, j, h
-!
-  DO WHILE( m <= 0 .OR. n <= 0 .OR. k <= 0 )
-    PRINT*, "Input array size m,n,k (>0) :"
-    READ*, m, n, k
+
+  DO WHILE (m <= 0 .OR. n <= 0 .OR. k <= 0)
+    PRINT *, "Input array size m,n,k (>0) :"
+    READ *, m, n, k
   END DO
-  ALLOCATE( a(m,n), b(n,k), c(m,k) ) ! c is given by a*b
+! I wanted to display a matrix form of a(i,j) here.
+!  DO i=1, m
+!    PRINT '(A, I2, A, I2, A)', ("a(", i, ",", j, ")", j=1,n)
+!  END DO   
+  ALLOCATE (a(m,n), b(n,k), c(m,k)) ! c will be given by a*b (matrix product)
 
-  PRINT*, "Input elements of A in a matrix form:"
-    READ*, ( ( a(i,j), j=1, n), i=1, m )
-  PRINT*, "Input elements of B in a matrix form:"
-    READ*, ( ( b(i,j), j=1, k), i=1, n )
+  PRINT *, "Input elements of A in a matrix form:"
+  READ *, ((a(i,j), j=1, n), i=1, m)
+  PRINT *, "A =" 
+  DO i=1, m
+    PRINT "(10I5)", (a(i,j), j=1, n)
+  END DO
 
+  PRINT *, "Input elements of B in a matrix form:"
+  READ *, ((b(i,j), j=1, k), i=1, n)
+  PRINT *, "B =" 
+  DO j=1, k
+    PRINT "(10I5)", (b(i,j), i=1, n)
+  END DO
+
+  PRINT *, "So, the matrix product of A and B is"
   DO i=1, m
     DO j=1, k
       c(i,j) = 0 ! initialize
@@ -25,7 +39,7 @@ PROGRAM Matrix
   END DO
 
   DO i=1, m
-    PRINT "(10I5)", ( c( i, j), j=1, k)
+    PRINT "(10I5)", (c(i,j), j=1, k)
   END DO
 
 END PROGRAM Matrix
