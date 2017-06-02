e0f09975ad9acc41cb07d1402f208ea724855c71
Ray D. Sameshima <rds@rds.local>
2017-06-02 10:41:29 +0900

    ex6_9 voter rule

--- Chapter06/ex6_9.f90
+++ Chapter06/ex6_9.f90
@@ -1,16 +1,19 @@
 ! ex6_9.f90
 ! Voter rule, like a life game
+! Under periodic boundary condition, a voter will make their decision
+! via their nearest neighborhood.
 PROGRAM VoterRule
   INTEGER :: voter(1:20), neighbors(1:20), i=0
 !  
-  PRINT*, "Input initial bits for members:"
-  READ*, voter(1:20)
+  PRINT *, "Input initial bits (i.e., 01 sequence) for members:"
+  READ *, voter(1:20)
   PRINT '(I3, ":",20I2)',i , voter(1:20)
 !
   DO i=1, 10 
-    neighbors = CSHIFT( voter, 1 ) + CSHIFT( voter, -1 )
-    WHERE( neighbors /= 1 ) voter = neighbors/2
+    neighbors = CSHIFT(voter, 1) + CSHIFT(voter, -1)
+! If 1 then use original bit, else change the mind with neighbors.
+    WHERE (neighbors /= 1) voter = neighbors/2
     Print '(I3, ":", 20I2)', i, voter(1:20)
   END DO
-!
+
 END PROGRAM VoterRule
