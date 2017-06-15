93860d5c4bb0089e70dc567222a97cd6fa892aa9
Ray D. Sameshima <rds@rds.local>
2017-06-15 16:43:41 +0900

    ex9_1 3-vectors

---
+++ Chapter09/ex9_1.f90
@@ -0,0 +1,26 @@
+! ex9_1.f90
+! Scalar and vector products of two 3-vectors.
+PROGRAM main
+  TYPE vector
+    REAL :: x,y,z
+  END TYPE vector
+  TYPE(vector) :: a,b,c
+  REAL :: s
+
+  PRINT*, "Input 3 components of the first vector:"
+  READ*, a
+  PRINT*, "Input 3 components of the second vector:"
+  READ*, b
+  s = a%x * b%x + a%y * b%y + a%z * b%z
+  PRINT "(' a.b = ', F6.3 )", s
+  c = vector_product(a,b)
+  PRINT "(' a^b = ', F6.3, ',', F6.3, ',', F6.3, ')' )", c
+
+CONTAINS
+  FUNCTION vector_product(u,v) RESULT(w)
+    TYPE(vector) :: u,v,w
+    w%x = u%y * v%z - u%z * v%y
+    w%y = u%z * v%x - u%x * v%z
+    w%z = u%x * v%y - u%y * v%x
+  END FUNCTION vector_product
+END PROGRAM main
